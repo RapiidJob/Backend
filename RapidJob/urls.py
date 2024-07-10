83dbb350b2674e6ffd53b7aae0a73678a8b8e23c
Abdulwahid <Abdulwahidhussen10@gmail.com>
2024-07-10 11:17:22 -0700

    added api/ infront of every urls

--- RapidJob/urls.py
+++ RapidJob/urls.py
@@ -23,20 +23,20 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.jwt')),
-    path('auth/', include('djoser.urls.authtoken')),
+    path('api/auth/', include('djoser.urls')),
+    path('api/auth/', include('djoser.urls.jwt')),
+    path('api/auth/', include('djoser.urls.authtoken')),
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    path('accounts/', include('accounts.urls')),
+    path('api/swagger', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('api/redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('api/accounts/', include('accounts.urls')),
     # path('jobs/', include('jobs.urls')),
-    path('messages/', include('messages.urls')),
+    path('api/messages/', include('messages.urls')),
     
     path('auth/users/reset_password_confirm/<uid>/<token>/', password_reset_confirm_view, name='password_reset_confirm'),
-    path("accounts/", include("accounts.urls")),
-    path('jobs/', include('jobs.urls')),
-    path('application/', include('applications.urls') ),
+    path("api/accounts/", include("accounts.urls")),
+    path('api/jobs/', include('jobs.urls')),
+    path('api/application/', include('applications.urls') ),
 ]
 
 if settings.DEBUG:
--- jobs/urls.py
+++ jobs/urls.py
@@ -6,7 +6,7 @@ from .views import (
 )
 
 urlpatterns = [
-    path('', JobListAPIView.as_view(), name='job-list'),
+    path('list', JobListAPIView.as_view(), name='job-list'),
     path('create/', JobCreateAPIView.as_view(), name='job-create'),
     path('<int:pk>/', JobRetrieveUpdateAPIView.as_view(), name='job-detail'),
     path('get/<int:pk>/', JobRetrieveAPIView.as_view(), name="retrive"),
